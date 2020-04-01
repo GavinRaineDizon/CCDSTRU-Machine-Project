7c77262fe8081787d2029a2e68a2d03609111412
GavinRaineDizon <60138634+GavinRaineDizon@users.noreply.github.com>
2020-04-02 00:01:39 +0800

    Update main.c

--- main.c
+++ main.c
@@ -48,8 +48,41 @@ void NextPlayerMove(String state[][3], int row, int col, int * cardinality, int 
 	}else{
 		strcpy(state[row-1][col-1], "FREE");
 		(*cardinality)--;
+		printf("[%d]\n");
 	}
 		
+}
+//made changes
+void GameOver(String state[][3], int * over){
+	
+	char result[20] = {'\0'};
+	char who[5];
+	
+	//DIAGONAL 1 WIN CONDITION
+	if(strcmp(state[0][0], state[1][1]) == 0 && strcmp(state[1][1], state[2][2]) == 0 && strcmp(state[2][2], "FREE") != 0)
+		strcpy(who, state[0][0]);
+	else if(strcmp(state[2][0], state[1][1]) == 0 && strcmp(state[1][1], state[2][0]) == 0 && strcmp(state[2][0], "FREE") != 0)
+		strcpy(who, state[2][0]);
+	else if(strcmp(state[0][1], state[1][1]) == 0 && strcmp(state[1][1], state[2][1]) == 0 && strcmp(state[2][1], "FREE") != 0)
+		strcpy(who, state[0][1]);
+	else if(strcmp(state[0][1], state[1][1]) == 0 && strcmp(state[1][1], state[2][1]) == 0 && strcmp(state[2][1], "FREE") != 0)
+		strcpy(who, state[0][1]);
+	else if(strcmp(state[1][0], state[1][1]) == 0 && strcmp(state[1][1], state[1][2]) == 0 && strcmp(state[1][2], "FREE") != 0)
+		strcpy(who, state[1][0]);
+	else
+		strcpy(who, "FREE");
+	if(strcmp(who, "FREE") == 0){	
+	}
+	else{
+		if(strcmp(who, "UNO") == 0){
+			strcpy(result, "UNO wins");
+			printf("\n%s\n\n", result);}
+		else{
+			strcpy(result, "DOS wins");
+			printf("\n%s\n\n", result);
+		}
+		*over = 1;
+	}		
 }
 
 int isCoordinateValid(String state[][3], int posRow, int posCol, int turn, int *turnCntPtr){
@@ -69,47 +102,44 @@ int main(){
 	int turn = 1;
 	
 //Turn Cardinalities
-	int Uno = 0; 
+	int Uno = 0;
 	int Dos = 0;
 	int *turnCntPtr; 
-
 	int posRow = 0;
 	int posCol = 0;
 	int valid;
-	int valid2;
-	initBoard(state);
 	
+	initBoard(state);
 	do{
 		turn = !turn;
-		valid = 1;
-		
 		if(turn)
 			turnCntPtr = &Dos;
 		else
 			turnCntPtr = &Uno;
 		
 		printBoard(state);
-
-		printf("\nIt is %s's turn\n", turn ? "Dos": "Uno");
+		printf("\nIt is %s turn\n", turn ? "Dos's": "Uno's");
 		
 		do{
-			do{	
-				if(!valid)
-					printf("INVALID");
-
+			do{
 				printf("\nEnter row coordinate [1-3]: ");
 				scanf("%d", &posRow);				
 			}while(posRow <= 0 || posRow >= 4);
 			do{
 				printf("\nEnter column coordinate[1-3]: ");
-				scanf("%d", &posCol);									
+				scanf("%d", &posCol);					
 			}while(posCol <= 0 || posCol >= 4);
-				valid = 0;
+		
 		}while(!isCoordinateValid(state, posRow, posCol, turn, turnCntPtr));
 	
-		//GameOver();
 		NextPlayerMove(state, posRow, posCol, turnCntPtr, turn);
-		system("cls");
+		//changed this
+	    GameOver(state, &over);
+	
+	    	system("pause");
+			system("cls");
+		
+		//until here
 	}while(!over);
 	
     return 0;
